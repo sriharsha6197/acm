1f62c3b1e0c2b803496466ccab4dc8f7ce34fef7
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-10-05 17:46:58 +0530

    acm creation

--- main.tf
+++ main.tf
@@ -13,3 +13,11 @@ resource "aws_acm_certificate" "cert" {
 variable "env" {
   
 }
+resource "aws_route53_record" "main" {
+  allow_overwrite = true
+  name            = "${var.env}.certificate"
+  records         = [aws_acm_certificate.cert.validation_method]
+  ttl             = 60
+  type            = "CNAME"
+  zone_id         = data.aws_route53_zone.selected.zone_id
+}
